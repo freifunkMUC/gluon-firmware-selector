7269bbc91dbf4240ee9e4c1dc0af9c3ada26493d
Grische <2787581+grische@users.noreply.github.com>
2025-07-12 13:53:04 +0200

    devices: fix ffmuc raspberry pi 4 name
    add current upstream name and remove old unused raspberry-4 name

--- devices.js
+++ devices.js
@@ -326,7 +326,7 @@ var devices_recommended = {
     "PI1": { "raspberry-pi": "", "raspberrypi-model-b": "" },
     "PI2": { "raspberry-pi-2": "", "raspberrypi-2-model-b": "" },
     "PI3": { "raspberry-pi-3": "", "raspberrypi-3-model-b": "" },
-    "PI4": { "raspberry-pi-4": "", "raspberry-pi-4-model-b": "" },
+    "PI4": { "raspberry-pi-4-model-b": "", "raspberrypi-4-model-b": "" },
   },
 
   "RAVPower": {
